3829bacb50183777c6d27817514d8497369c9122
Natalia Mackowiak <nat.mackowiak@gmail.com>
2023-05-03 15:17:26 -0700

    Init

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import VueRouter from 'vue-router';
+// Pages
+
+import HomeTemplate from "./ideas/_template/HomeTemplate";
+import RegisterTemplate from "./ideas/_template/RegisterTemplate";
+
+export const router = new VueRouter({
+    mode: 'history',
+    linkExactActiveClass: 'active',
+    scrollBehavior() {
+      return { x: 0, y: 0 };
+    },
+    routes: [
+      { path: '/', component: HomeTemplate },
+      { path: '/register', component: RegisterTemplate },
+      
+      // otherwise redirect to home
+      { path: '*', redirect: '/' }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    // redirect to login page if not logged in and trying to access a restricted page
+
+    next();
+})
